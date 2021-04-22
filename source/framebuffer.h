3bc2cdcfb57901e846421766dcb6c4c3bf0fb858
monica-nguyen <monica.nguyen1@ucalgary.ca>
2021-04-21 21:04:47 -0600

    Fixed bugs preventing compilation

--- source/framebuffer.h
+++ source/framebuffer.h
@@ -2,14 +2,17 @@
 #define FRAMEBUFF
 
 /* definitions */
-struct fbs initFbInfo(void) {
+struct fbs {
 	char *fptr;		// framebuffer pointer
 	int xOff; 					// x offset
-	int yOff;					// y offset
+	int yOff;					// x offset
 	int bits;					// bits per pixel
 	int lineLength;				// Line Length
 	float screenSize;			// Screen Size
 };
+	
+struct fbs initFbInfo(void);
+
 
 
 #endif
